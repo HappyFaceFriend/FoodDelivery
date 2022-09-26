b84bfb06e8c63979df4a910ea3b9df21fc275557
GoAheadMan <hataway072@gmail.com>
2022-09-26 15:51:42 +0900

    Dash 기능 구현

--- Assets/Scripts/Player/DashState.cs
+++ Assets/Scripts/Player/DashState.cs
@@ -1,25 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveState : PlayerState
+public class DashState : PlayerState
 {
-    public MoveState(PlayerBehaviour player) : base("Move", player)
+    public DashState(PlayerBehaviour player) : base("Dash", player)
     {
     }
     public override void OnUpdate()
     {
         base.OnUpdate();
 
-        if (Input.GetKey(KeyCode.E))
-            Player.ChangeState(new DashState(Player));
-
+        if (Input.GetKeyUp(KeyCode.E))
+            Player.ChangeState(new MoveState(Player));
 
         Vector3 moveDir = Player.GetInputVector();
 
-        transform.Translate(moveDir * Player.MoveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(moveDir * Player.MoveSpeed * Time.deltaTime * 2, Space.World);
 
-        if (moveDir != Vector3.zero)
+        if (moveDir != Vector3.zero )
         {
             Quaternion toRotation = Quaternion.LookRotation(moveDir, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, Player.RotateSpeed * Time.deltaTime);
--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -28,5 +28,6 @@ public class PlayerBehaviour : StateMachineBase
         {
             Animator.SetTrigger("Attack");
         }
+        
     }
 }
