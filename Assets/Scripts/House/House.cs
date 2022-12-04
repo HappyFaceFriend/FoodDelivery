76c6657edbfba7818eb8fb3b8375895663c24b65
GoAheadMan <hataway072@gmail.com>
2022-12-04 17:52:25 +0900

    to other house

--- Assets/Scripts/House/House.cs
+++ Assets/Scripts/House/House.cs
@@ -8,7 +8,6 @@ public class House : ScriptableObject
 {
     public string HouseName;
     public Sprite Icon;
-    public bool ordercheck = true;
     public void Introduce()
     {
         Debug.Log("Áý ÀÌ¸§ : " + HouseName);
